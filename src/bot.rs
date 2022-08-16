58af72454bf74d404f935544508e805f7bab4c72
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-16 15:38:01 +0200

    Setup the main function

--- src/bot.rs
+++ src/bot.rs
@@ -1,8 +1,12 @@
 use serde::{self, Deserialize, Serialize};
-use serde_json::{Value};
-use tokio::{task::JoinHandle, fs::{File, self, OpenOptions}, io::AsyncWriteExt};
-use std::{env, thread, time::Duration, path::Path};
+use serde_json::Value;
 use std::collections::HashMap;
+use std::{env, path::Path, thread, time::Duration};
+use tokio::{
+    fs::{self, File, OpenOptions},
+    io::AsyncWriteExt,
+    task::JoinHandle,
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Chat {
@@ -28,19 +32,22 @@ pub struct Update {
     message: Message,
 }
 
-pub struct Bot {
-    telegram_endpoint: String,
-    exchange_rates_endpoint: String,
-}
+pub struct Bot;
+
 
 impl Bot {
-    pub fn new() -> Self {
+    pub fn get_endpoints() -> (String, String) {
         let bot_auth_token = env::var("BOT_AUTH_TOKEN").expect("BOT_AUTH_TOKEN is missing");
-        let exchange_rate_token = env::var("EXCHAINGE_RATES_API_TOKEN").expect("EXCHAINGE_RATE_API_TOKEN is missing");
-        Bot {
-            telegram_endpoint: format!("https://api.telegram.org/bot{}", bot_auth_token),
-            exchange_rates_endpoint: format!("https://openexchangerates.org/api/latest.json?app_id={}", exchange_rate_token)
-        }
+        let exchange_rate_token =
+            env::var("EXCHAINGE_RATES_API_TOKEN").expect("EXCHAINGE_RATE_API_TOKEN is missing");
+
+        let telegram_endpoint = format!("https://api.telegram.org/bot{}", bot_auth_token);
+        let exchange_rates_endpoint = format!(
+            "https://openexchangerates.org/api/latest.json?app_id={}",
+            exchange_rate_token
+        );
+
+        (telegram_endpoint, exchange_rates_endpoint)
     }
 
     /// Offical docs for this endpoint https://core.telegram.org/bots/api#getupdates
@@ -57,26 +64,49 @@ impl Bot {
         updates
     }
 
-    pub async fn send_message(&self, msg: &MessageBody) {
-        let api = format!("{}/sendMessage", self.telegram_endpoint);
+    // Docs https://core.telegram.org/bots/api#sendmessage
+    pub async fn send_message(msg: &MessageBody) {
+        let (telegram_endpoint, _) = Bot::get_endpoints();
+        let api = format!("{}/sendMessage", telegram_endpoint);
         let client = reqwest::Client::new();
         let _ = client.post(api).json(&msg).send().await.unwrap();
     }
 
-    pub async fn send_exchange_rates_msg(&self, chat_id: &str) {
-        let rates = self.get_exchange_rates().await;
+    pub async fn send_exchange_rates_msg(chat_id: &str) {
+        let rates = Bot::get_exchange_rates().await;
         let msg = MessageBody::new_exchange_rates_msg(chat_id, &rates);
-        self.send_message(&msg).await;
+        Bot::send_message(&msg).await;
     }
 
-    pub async fn get_exchange_rates(&self) -> ExchangeRates {
-        let res = reqwest::get(&self.exchange_rates_endpoint).await.unwrap();
+    pub async fn get_exchange_rates() -> ExchangeRates {
+        let (_, exchange_rates_endpoint) = Bot::get_endpoints();
+        let res = reqwest::get(exchange_rates_endpoint).await.unwrap();
         let body = res.text().await.unwrap();
         let rates: ExchangeRates = serde_json::from_str(&body).unwrap();
-        
+
         rates
     }
 
+    pub async fn bradcast_updates() {
+        loop {
+            let chats = Bot::load_bot_data().await;
+            let mut hanlders = vec![];
+            for chat in chats {
+                let handler = tokio::spawn(async move {
+                    Bot::send_exchange_rates_msg(&format!("{}", chat.id)).await;
+                });
+
+                hanlders.push(handler)
+            }
+
+            for handler in hanlders {
+                handler.await.unwrap()
+            }
+
+            // Broadcast every 30min
+            thread::sleep(Duration::from_secs(50 * 30))
+        }
+    }
 
     pub fn poll_updates() -> JoinHandle<()> {
         tokio::spawn(async {
@@ -99,22 +129,25 @@ impl Bot {
                 Bot::update_bot_data(chats).await;
                 thread::sleep(Duration::from_secs(6)); // Every 10 mins
 
-                iter+=1
+                iter += 1
             }
         })
     }
 
-
     pub async fn load_bot_data() -> Vec<Chat> {
         let path = Path::new("data.json");
         if !path.exists() {
-            File::create(path).await.expect("Unable to create data.json");
+            File::create(path)
+                .await
+                .expect("Unable to create data.json");
         }
 
-        let content = fs::read_to_string(path).await.expect("Failed to read the file");
+        let content = fs::read_to_string(path)
+            .await
+            .expect("Failed to read the file");
 
         if content.is_empty() {
-            return vec![]
+            return vec![];
         }
 
         let chats: Vec<Chat> = serde_json::from_str(&content).unwrap();
@@ -122,18 +155,16 @@ impl Bot {
         chats
     }
 
-
     pub async fn update_bot_data(chats: Vec<Chat>) {
         let path = Path::new("data.json");
         let mut f = OpenOptions::new().write(true).open(path).await.unwrap();
         let json_str = serde_json::to_string(&chats).unwrap();
         f.write_all(json_str.as_bytes()).await.unwrap();
     }
-
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct MessageBody {
+pub struct MessageBody {
     chat_id: String,
     text: String,
     parse_mode: &'static str,
@@ -149,12 +180,15 @@ impl MessageBody {
     }
 
     pub fn new_exchange_rates_msg(chat_id: &str, rates: &ExchangeRates) -> MessageBody {
-        let text = format!(r#"
+        let text = format!(
+            r#"
         *Prices updates*
         1 USD = {} EGP
         1 USD = {} SAR
-        "#, rates.rates.egp, rates.rates.sar);
-        
+        "#,
+            rates.rates.egp, rates.rates.sar
+        );
+
         MessageBody::new(chat_id, &text)
     }
 }
@@ -180,7 +214,7 @@ impl PraseMode {
 pub struct ExchangeRates {
     timestamp: i64,
     base: String,
-    rates: Rates
+    rates: Rates,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -190,4 +224,3 @@ pub struct Rates {
     #[serde(rename = "SAR")]
     sar: f64,
 }
-
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,12 @@ use dotenv::dotenv;
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     dotenv().ok();
-    let bot = Bot::new();
-
-    // bot.get_updates().await;
-    // bot.send_message().await;
-    // bot.get_exchange_rates().await;
-    Bot::poll_updates().await.unwrap();
+    let updates_hanlder = Bot::poll_updates();
+    Bot::bradcast_updates().await;
+    updates_hanlder.await.unwrap();
     Ok(())
 }
 
-// Todo: 1. Error handling 
+// Todo: 1. Error handling
 // Todo: 2. save user data into json file
 // Todo: 3. Extened the logic to include gold as well
