d10db6f6f314344b362a61aa9b53460dbc13ffed
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-16 16:51:01 +0200

    Fix typo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "pokemon",
-      script: "./target/release/ehco",
+      script: "./target/release/echo",
       exec_interpreter: "none",
       exec_mode: "fork_mode",
     },
